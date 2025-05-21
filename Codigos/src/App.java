f024191c77ff134440a9178ae10522c1d2ebb204
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-05-21 08:11:12 -0600

    Vamos con los string y ponemos 3 JoptionPane para mostrar mensaje y que el usuario pueda ingresar 3 y sumarlos

---
+++ Codigos/src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        String x = JOptionPane.showInputDialog("Dame el primer numero");
+        String y = JOptionPane.showInputDialog("Dame el segundo numero");
+        String z = JOptionPane.showInputDialog("Dame el tercer numero");
+
+    }
+}
